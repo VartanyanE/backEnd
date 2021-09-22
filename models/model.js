3142c2c3d5f998141198a786fdd9dfde8aa95d53
VartanyanE <mike.vartanyan@yahoo.com>
2021-09-21 18:30:38 -0700

    appended model

--- models/model.js
+++ models/model.js
@@ -3,7 +3,8 @@ import mongoose  from "mongoose";
 const mainSchema = mongoose.Schema({
     website: "String",
     username: "String",
-    password: "String"
+    password: "String",
+    selectedFile: "String"
 });
 
 const mainModel = mongoose.model("Main", mainSchema);
